9cb8e7f9ae1b664066de962b60e4739cdc7b9ffa
S_Maslinikov <maslinikovsergey@gmail.com>
2026-02-05 18:11:08 -0500

    .

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    """
+    Purpose: Call all modules in the correct order to execute the program
+    
+    High-level steps:
+    - Call user_input module
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ modules/scrolling_output.py
@@ -0,0 +1,12 @@
+def scrolling_output():
+    """
+        Purpose: Create image and table output for user
+        Input: Scoring matrix file 
+        Output: Image (likely .png), and a file (.csv or .tsv) of top three choices 
+        High-level steps: 
+        -    Create image formatting for the raw input data alongside the scoring and optimal traceback paths 
+        -    Export the image in a viewable, legible format 
+            .png, jpeg, .pdf 
+
+    """
+    pass
