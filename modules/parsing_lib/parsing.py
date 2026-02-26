8987e6a1282685be95144060c0360ed26b4fc842
S_Maslinikov <maslinikovsergey@gmail.com>
2026-02-26 15:59:31 -0500

    back
    commit bacj

--- modules/parsing_lib/parsing.py
+++ modules/parsing_lib/parsing.py
@@ -13,43 +13,6 @@ def parsing(x, y, pad=1):
         -    Be able to rank the top three choices(?), and have tie-rank capabilities 
 
     """
-    xs = [str(v) for v in x]
-    ys = [str(v) for v in y]
-
-    col_widths = [max(1, len(h)) for h in xs]
-    row_header_width = max((len(s) for s in ys), default=0)
-
-    def hor(w):
-        return '-' * (w + pad * 2)
-
-    parts = [hor(row_header_width)] + [hor(w) for w in col_widths]
-    top_border = '+' + '+'.join(parts) + '+'
-
-    def cell_content(text, width):
-        return ' ' * pad + (text or '').center(width) + ' ' * pad
-
-    header_cells = [cell_content('', row_header_width)] + [cell_content(xs[i], col_widths[i]) for i in range(len(xs))]
-    header_line = '|' + '|'.join(header_cells) + '|'
-
-    sep = top_border
-
-    rows = []
-    for yi in ys:
-        row_header_cell = ' ' * pad + yi.ljust(row_header_width) + ' ' * pad
-        empty_cells = [cell_content('', w) for w in col_widths]
-        row_line = '|' + '|'.join([row_header_cell] + empty_cells) + '|'
-        rows.append(row_line)
-
-    print(top_border)
-    if xs or row_header_width:
-        print(header_line)
-        print(sep)
-    for r in rows:
-        print(r)
-        print(sep)
-
-parsing(['A','B'], ['A','B'])
-
-    
+    pass
 
 #Sergey will be doing the parsing function(s)
