714511df14acd9e6e17ac8ffd69d0640a4f28b4b
S_Maslinikov <maslinikovsergey@gmail.com>
2026-02-26 17:44:25 -0500

    parsing first step
    parsing first step

--- modules/parsing_lib/parsing.py
+++ modules/parsing_lib/parsing.py
@@ -1,4 +1,4 @@
-def parsing(x, y, pad=1):
+def parsing(file_from_input):
     """
         Purpose: Puts the two DNA sequences in a table-like array
         Input: Taking the two DNA sequences from the User Input File output
@@ -13,6 +13,32 @@ def parsing(x, y, pad=1):
         -    Be able to rank the top three choices(?), and have tie-rank capabilities 
 
     """
-    pass
+    sequences = []
+    current_id = None
+    current_seq = []
+    
+    """next step is use real fasta files to test the parsing function, and make sure it can handle multiple 
+    sequences in one file, and that it can handle sequences of different lengths.
+    Also need to make sure it can handle sequences with different characters (e.g. N for unknown bases)."""
+
+    with open(file_from_input, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith('>'):
+                if current_id is not None:
+                    sequences.append((current_id, ''.join(current_seq)))
+                current_id = line[1:]
+                current_seq = []
+            else:
+                current_seq.append(line.upper())
+
+    if current_id is not None:
+        sequences.append((current_id, ''.join(current_seq)))
+
+    return sequences
+
+
 
 #Sergey will be doing the parsing function(s)
