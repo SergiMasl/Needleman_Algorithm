a2609d61934e21f3bcf6fa5bbb3ff16bcf9b84d8
S_Maslinikov <maslinikovsergey@gmail.com>
2026-02-26 18:17:20 -0500

    fixed user input
     User input for match score, mismatch score, and gap penalty

--- modules/parsing_lib/get_scoring_parameters.py
+++ modules/parsing_lib/get_scoring_parameters.py
@@ -4,10 +4,23 @@ def get_scoring_parameters():
         Input: User input for match score, mismatch score, and gap penalty
         Output: Match score, mismatch score, and gap penalty as integers
     """
-    match_score = int(input("Enter the match score: "))
-    mismatch_score = int(input("Enter the mismatch score: "))
-    gap_penalty = int(input("Enter the gap penalty: "))
+    print("default values for match score, mismatch score, and gap penalty are +2, -1, and -2 respectively.")
+    print("You can choose to use the default values or enter your own values.")
+    user_parameters = input("Do you want to use the default values? (yes/no): ")
+    while user_parameters.lower() not in ["yes", "no"]:
+        user_parameters = input("Invalid input. Please enter 'yes' or 'no': ")
+    
+    if user_parameters.lower() == "yes":
+        match_score = 2
+        mismatch_score = -1
+        gap_penalty = -2
+    else:
+        match_score = int(input("Enter the match score: "))#+2
+        mismatch_score = int(input("Enter the mismatch score: "))#-1
+        gap_penalty = int(input("Enter the gap penalty: "))#-2
     
     return match_score, mismatch_score, gap_penalty
 
-get_scoring_parameters()
+#checking:
+tets_1 = get_scoring_parameters()
+print(tets_1)
--- modules/parsing_lib/parsing.py
+++ modules/parsing_lib/parsing.py
@@ -1,6 +1,8 @@
 def parsing(file_from_input):
     """
      -this function will take one array which contain 2 seqs arrays
+     - step 1: ask user asking user for match score, mismatch score, and gap penalty
+     - step 2: put the two DNA sequences in a table-like array, and calculate the scoring values for the matrix, and score the values properly and be able to tabulate properly,
 
         Purpose: Puts the two DNA sequences in a table-like array
         Input: Taking the two DNA sequences from the User Input File output
@@ -15,10 +17,6 @@ def parsing(file_from_input):
         -    Be able to rank the top three choices(?), and have tie-rank capabilities 
 
     """
-    
-    """next step is use real fasta files to test the parsing function, and make sure it can handle multiple 
-    sequences in one file, and that it can handle sequences of different lengths.
-    Also need to make sure it can handle sequences with different characters (e.g. N for unknown bases)."""
 
     ###change to match and mismatch scoring values, and gap penalties, and make sure it can handle those properly in the scoring matrix and traceback functions.
 
