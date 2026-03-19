c5b738ac3977bb29c5cf7f1f8786cafaed977887
escott60 <escott60@charlotte.edu>
2026-03-19 18:34:59 -0400

    Update main.py
    Added the CLI and made sure to initialize parameters!
    Imports will be finalized next week and the last part of the project is to make sure all output goes to the terminal properly.

--- main.py
+++ main.py
@@ -1,14 +1,98 @@
-def main():
-    """
-    Purpose: Call all modules in the correct order to execute the program
-    
-    High-level steps:
-    - Call user_input module
-    """
-    print("Welcome to the program!")
-    pass
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse 
+from 
+#Updated (3/19/2026)
+
+#make an argument parser to define some CLI options, such as the minimum and
+#maximum lengths of sequence inputs. Make CLI options for the match, mismatch,
+# and gap penalties 
+
+#MAKE SURE TO IMPORT ALL OTHER LIBRARY MODULES AND NECESSARY EXTENSION MODULES 
+
+#3/19/2026 Added CLI and a class to the main function. Main() will be constructed by
+#all team members and making sure all inputs are passed correctly
+class CreateAlignment: 
+
+	def __init__(
+		self, 
+		input_file: str= None,
+		sequence1: str= None, 
+		sequence2: str= None,
+		match:int = 2, 
+		mismatch:int = -1, 
+		gap:int = -2
+		) -> None 
+
+	def create_parser() -> argparse.ArgumentParser: 
+
+		#Setting the scoring scheme to a default (modify as a last improvement):
+		#Match = +2
+		#Mismatch = -1
+		#Gap Penalty = -2
+
+		parser = argparse.ArgumentParser(description="Needleman-Wunsch CLI") 
+		parser.add_argument( 
+			"-i", 
+			"--firstseq", 
+			type=str ,
+			help="Input Sequence #1", 
+			)
+
+		parser.add_argument( 
+			"-j", 
+			"--secondseq", 
+			type=str ,
+			help="Input Sequence #2", 
+			)
+
+		parser.add_argument( 
+			"-f", 
+			"--inputfasta",
+			default = "sequences.fasta", 
+			type=str,
+			help="Input Fasta File", 
+			)
+
+		parser.add_argument( 
+			"-m", 
+			"--match", 
+			type=int,
+			default=2,
+			help="Input Fasta File", 
+			)
+
+		parser.add_argument( 
+			"-n", 
+			"--mismatch", 
+			type=int,
+			default=-1,
+			help="Input Fasta File", 
+			)
+
+		parser.add_argument( 
+			"-g", 
+			"--gapscore", 
+			type=int,
+			default=-2,
+			help="Input Fasta File", 
+			)
+
+		parser.add_argument( 
+			"-o", 
+			"--outputfasta", 
+			type=str,
+			default="nwalignment.fasta",
+			help="Output Fasta File", 
+			)
+
+
+	def main() -> None 
+
+
 
 if __name__ == "__main__":
-    main()
-
-#Mehrnoush will be doing the main function 
+	main()
